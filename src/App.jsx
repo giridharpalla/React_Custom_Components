25f020110e821d0950a409e36aa5a5b8b9425acd
giridharpalla <68072049+giridharpalla@users.noreply.github.com>
2024-08-22 12:03:16 +0530

    added a dynamic function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+const randomGenerator = ['Fundametnal', 'Core', 'Conceptual'];
+
+function Number(max){
+  return Math.floor(Math.random()*(max+1));
+}
+
+
 function Header(){
   return (
   <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {randomGenerator[Number(2)]} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>);
